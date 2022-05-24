7d331b3934e5593dd3befce7c0ee8a3245a3cdb0
Albert Leng Chan Seng <albertleng@yahoo.com>
2022-05-24 09:17:37 +0800

    Added Square function

---
+++ v2/arithmetic.go
@@ -0,0 +1,18 @@
+package arithmetic
+
+// Check if a number is prime or not
+func IsPrime(num int) bool {
+	if num == 1 {
+		return false
+	}
+	for i := 2; i < int(num/2); i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func Square(num int) int {
+	return num * num
+}
---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/albertleng/arithmetic/v2
+
+go 1.17
